70d2f39ef494aa65796f58408efd0a4e37248f30
Krip Phongphun <krip.phongphun@gmail.com>
2019-08-27 16:01:37 +0700

    Add login page

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// When the user clicks the button, open the modal
+function loginButton() {
+  // Get the button that opens the modal
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function dismissButton() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+function TestLogin() {
+  console.log("Login Button Active.");
+}
