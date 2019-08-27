1a8ba3d9c95f6f7907818213ff2100bdb7f9a81f
Krip Phongphun <krip.phongphun@gmail.com>
2019-08-27 22:10:14 +0700

    Edit POST method by jquery

--- js/login.js
+++ js/login.js
@@ -12,47 +12,42 @@ function dismissButton() {
 }
 
 function TestLogin() {
-  console.log("Login Button Active.");
-  location.href = "index.html";
+  // console.log("Login Button Active.");
+  // location.href = "index.html";
 
-//  POST method by jQuery
-//   jQuery.ajax({
-//
-//       // url: "https://peahub21.azurewebsites.net/api/login/",
-//       url: "http://127.0.0.1:8000/api/v2.0/login/",
-//       type: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//
-//       data: JSON.stringify({
-//         "username": document.getElementById("username").value,
-//         "password": document.getElementById("password").value
-//       })
-//     })
-//     .done(function(data, textStatus, jqXHR) {
-//       console.log("HTTP Request Succeeded: " + jqXHR.status);
-//       console.log(data);
-//       if (jqXHR.status == 200) {
-//         console.log(data['type'])
-//         console.log("gggggggg")
-//         localStorage.setItem("token", data['token']);
-//         localStorage.setItem("comp_id", data['comp_id']);
-//         localStorage.setItem("comp_name", data['comp_name']);
-//         if (data['type'] == 'PER' || data['type'] == 'LGE') {
-//           window.location.replace("c11search.html")
-//         } else if (data['type'] == 'PEA') {
-//           window.location.replace("p11searchp.html")
-//         }
-//       }
-//     })
-//     .fail(function(jqXHR, textStatus, errorThrown) {
-//       console.log("HTTP Request Failed");
-//     })
-//     .always(function() {
-//       /* ... */
-//     });
-//   //
+  //  POST method by jQuery
+  console.log("POST method by jQuery");
+  jQuery.ajax({
+
+      // url: "https://peahub21.azurewebsites.net/api/login/",
+      url: "https://hookb.in/VGV2yLrM7dFkmkbNBqZr",
+      type: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      data: JSON.stringify({
+        "username": document.getElementById("username").value,
+        "password": document.getElementById("password").value
+      })
+    })
+    .done(function(data, textStatus, jqXHR) {
+      console.log("HTTP Request Succeeded: " + jqXHR.status);
+      console.log(data);
+      if (jqXHR.status == 200) {
+        console.log(data['type'])
+        console.log("gggggggg")
+        localStorage.setItem("token", data['token']);
+        window.location.replace("index.html")
+      }
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log("HTTP Request Failed");
+    })
+    .always(function() {
+      /* ... */
+    });
+  //
 }
 
 window.onclick = function(event) {
