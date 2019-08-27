44ebc4ad5e7e71112990f546d5e151506b32e746
Krip Phongphun <krip.phongphun@gmail.com>
2019-08-27 21:59:49 +0700

    Add POST method by jquery

--- js/login.js
+++ js/login.js
@@ -14,12 +14,51 @@ function dismissButton() {
 function TestLogin() {
   console.log("Login Button Active.");
   location.href = "index.html";
+
+//  POST method by jQuery
+//   jQuery.ajax({
+//
+//       // url: "https://peahub21.azurewebsites.net/api/login/",
+//       url: "http://127.0.0.1:8000/api/v2.0/login/",
+//       type: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//
+//       data: JSON.stringify({
+//         "username": document.getElementById("username").value,
+//         "password": document.getElementById("password").value
+//       })
+//     })
+//     .done(function(data, textStatus, jqXHR) {
+//       console.log("HTTP Request Succeeded: " + jqXHR.status);
+//       console.log(data);
+//       if (jqXHR.status == 200) {
+//         console.log(data['type'])
+//         console.log("gggggggg")
+//         localStorage.setItem("token", data['token']);
+//         localStorage.setItem("comp_id", data['comp_id']);
+//         localStorage.setItem("comp_name", data['comp_name']);
+//         if (data['type'] == 'PER' || data['type'] == 'LGE') {
+//           window.location.replace("c11search.html")
+//         } else if (data['type'] == 'PEA') {
+//           window.location.replace("p11searchp.html")
+//         }
+//       }
+//     })
+//     .fail(function(jqXHR, textStatus, errorThrown) {
+//       console.log("HTTP Request Failed");
+//     })
+//     .always(function() {
+//       /* ... */
+//     });
+//   //
 }
 
 window.onclick = function(event) {
   var modal = document.getElementById("myModal");
-      if (event.target == modal) {
-        console.log("Dismiss Modal");
-        modal.style.display = "none";
-      }
-    }
+  if (event.target == modal) {
+    console.log("Dismiss Modal");
+    modal.style.display = "none";
+  }
+}
