da8fd6140de30b2cd97866e13ab87cff187fd0fd
Krip Phongphun <krip.phongphun@gmail.com>
2019-10-08 11:05:14 +0700

    Fix energy chart js

--- js/energy.js/Chart.js
+++ js/energy.js/Chart.js
@@ -1,147 +1,142 @@
-// window.onload = function () {
-
-//     var chart = new CanvasJS.Chart("chartContainer1", {
-//         animationEnabled: true,
-//         exportEnabled: true,
-//         title: {
-//             // text: "MSA Control Building Energy Report" 
-//         },
-//         axisY: {
-//             title: "Number of Medals"
-//         },
-//         toolTip: {
-//             shared: true
-//         },
-//         legend: {
-//             cursor: "pointer",
-//             itemclick: toggleDataSeries
-//         },
-//         data: [{
-//             type: "spline",
-//             name: "Grid",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 44 },
-//                 { label: "Sydney 2000", y: 37 },
-//                 { label: "Athens 2004", y: 36 },
-//                 { label: "Beijing 2008", y: 36 },
-//                 { label: "London 2012", y: 46 },
-//                 { label: "Rio 2016", y: 46 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "PV",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 16 },
-//                 { label: "Sydney 2000", y: 28 },
-//                 { label: "Athens 2004", y: 32 },
-//                 { label: "Beijing 2008", y: 48 },
-//                 { label: "London 2012", y: 38 },
-//                 { label: "Rio 2016", y: 26 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "Total Load",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 1 },
-//                 { label: "Sydney 2000", y: 11 },
-//                 { label: "Athens 2004", y: 9 },
-//                 { label: "Beijing 2008", y: 19 },
-//                 { label: "London 2012", y: 29 },
-//                 { label: "Rio 2016", y: 27 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "1st Floor Load",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 26 },
-//                 { label: "Sydney 2000", y: 32 },
-//                 { label: "Athens 2004", y: 28 },
-//                 { label: "Beijing 2008", y: 22 },
-//                 { label: "London 2012", y: 20 },
-//                 { label: "Rio 2016", y: 19 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "2nd Floor Load",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 7 },
-//                 { label: "Sydney 2000", y: 8 },
-//                 { label: "Athens 2004", y: 9 },
-//                 { label: "Beijing 2008", y: 13 },
-//                 { label: "London 2012", y: 13 },
-//                 { label: "Rio 2016", y: 9 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "EBD Load",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 20 },
-//                 { label: "Sydney 2000", y: 13 },
-//                 { label: "Athens 2004", y: 13 },
-//                 { label: "Beijing 2008", y: 16 },
-//                 { label: "London 2012", y: 11 },
-//                 { label: "Rio 2016", y: 17 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "Precision A/C",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 20 },
-//                 { label: "Sydney 2000", y: 21 },
-//                 { label: "Athens 2004", y: 22 },
-//                 { label: "Beijing 2008", y: 23 },
-//                 { label: "London 2012", y: 24 },
-//                 { label: "Rio 2016", y: 28 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             name: "Battrry",
-//             showInLegend: true,
-//             dataPoints: [
-//                 { label: "Atlanta 1996", y: 50 },
-//                 { label: "Sydney 2000", y: 40 },
-//                 { label: "Athens 2004", y: 30 },
-//                 { label: "Beijing 2008", y: 20 },
-//                 { label: "London 2012", y: 10 },
-//                 { label: "Rio 2016", y: 30 }
-//             ]
-//         },
-
-//         ]
-//     });
-
-//     chart.render();
-
-//     function toggleDataSeries(e) {
-//         if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-//             e.dataSeries.visible = false;
-//         }
-//         else {
-//             e.dataSeries.visible = true;
-//         }
-//         chart.render();
-//     }
-
-// }
-
-
 window.onload = function () {
 
-    var chart = new CanvasJS.Chart("chartContainer2", {
+    var chart_1 = new CanvasJS.Chart("chartContainer1", {
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+            // text: "MSA Control Building Energy Report"
+        },
+        axisY: {
+            title: "Number of Medals"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "spline",
+            name: "Grid",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 44 },
+                { label: "Sydney 2000", y: 37 },
+                { label: "Athens 2004", y: 36 },
+                { label: "Beijing 2008", y: 36 },
+                { label: "London 2012", y: 46 },
+                { label: "Rio 2016", y: 46 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "PV",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 16 },
+                { label: "Sydney 2000", y: 28 },
+                { label: "Athens 2004", y: 32 },
+                { label: "Beijing 2008", y: 48 },
+                { label: "London 2012", y: 38 },
+                { label: "Rio 2016", y: 26 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "Total Load",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 1 },
+                { label: "Sydney 2000", y: 11 },
+                { label: "Athens 2004", y: 9 },
+                { label: "Beijing 2008", y: 19 },
+                { label: "London 2012", y: 29 },
+                { label: "Rio 2016", y: 27 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "1st Floor Load",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 26 },
+                { label: "Sydney 2000", y: 32 },
+                { label: "Athens 2004", y: 28 },
+                { label: "Beijing 2008", y: 22 },
+                { label: "London 2012", y: 20 },
+                { label: "Rio 2016", y: 19 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "2nd Floor Load",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 7 },
+                { label: "Sydney 2000", y: 8 },
+                { label: "Athens 2004", y: 9 },
+                { label: "Beijing 2008", y: 13 },
+                { label: "London 2012", y: 13 },
+                { label: "Rio 2016", y: 9 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "EBD Load",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 20 },
+                { label: "Sydney 2000", y: 13 },
+                { label: "Athens 2004", y: 13 },
+                { label: "Beijing 2008", y: 16 },
+                { label: "London 2012", y: 11 },
+                { label: "Rio 2016", y: 17 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "Precision A/C",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 20 },
+                { label: "Sydney 2000", y: 21 },
+                { label: "Athens 2004", y: 22 },
+                { label: "Beijing 2008", y: 23 },
+                { label: "London 2012", y: 24 },
+                { label: "Rio 2016", y: 28 }
+            ]
+        },
+        {
+            type: "spline",
+            name: "Battrry",
+            showInLegend: true,
+            dataPoints: [
+                { label: "Atlanta 1996", y: 50 },
+                { label: "Sydney 2000", y: 40 },
+                { label: "Athens 2004", y: 30 },
+                { label: "Beijing 2008", y: 20 },
+                { label: "London 2012", y: 10 },
+                { label: "Rio 2016", y: 30 }
+            ]
+        },
+
+        ]
+    });
+
+    chart_1.render();
+
+    function toggleDataSeries(e) {
+        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        }
+        else {
+            e.dataSeries.visible = true;
+        }
+        chart_1.render();
+    }
+
+    var chart_2 = new CanvasJS.Chart("chartContainer2", {
         animationEnabled: true,
         exportEnabled: true,
         title: {
@@ -199,7 +194,7 @@ window.onload = function () {
         }]
     });
 
-    chart.render();
+    chart_2.render();
 
     function toggleDataSeries(e) {
         if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
@@ -208,7 +203,8 @@ window.onload = function () {
         else {
             e.dataSeries.visible = true;
         }
-        chart.render();
+        chart_2.render();
     }
+
 
 }
