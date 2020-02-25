612a5bbfdce0e59131cd8d1df209d8c2c33e74a3
Krip Phongphun <krip.phongphun@gmail.com>
2020-02-25 21:58:36 +0700

    Fix URL Plug and fix AC logic

--- js/bedroom.js/air_control.js
+++ js/bedroom.js/air_control.js
@@ -1,36 +1,28 @@
 // On
-function turn_on_ac() {
-    console.log("Hello");
+function turn_on_ac(device_id) {
+    console.log("turn_on_ac" + device_id);
     //
     console.log("POST method by jQuery");
     jQuery.ajax({
-        // url: "https://cors-anywhere.herokuapp.com/https://peahivebackend.herokuapp.com/hiveapi/",
-        url: "https://cors-anywhere.herokuapp.com/https://hookb.in/2qMV6mpxeDiDYDK60pRq",
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
         type: "POST",
         headers: {
-            // "Authorization": "Token d6c0e1dd9df3d71a3b21d70959bf0857859c4199",
-            "Authorization": "Token dscnsjdkcnsjdkcnjsdcnjdsncjsdnckjsdnckjsdcnkjsdcnskjdcnjksdncbs",
-            "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://hookb.in/2qMV6mpxeDiDYDK60pRq",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
             "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
-        contentType: "application/json",
-        data: JSON.stringify({
-            "topic": "Test post device control",
-            "type": "devicecontrol",
-            "message": {
-                "device": "MSA Device control",
-                "parameter": {
-                    "status": "on"
-                }
-            }
-        })
+        contentType: "application/x-www-form-urlencoded",
+        data: {
+            "device_type": "AC",
+            "device_id": device_id,
+            "status": "ON",
+        },
     })
         .done(function (data, textStatus, jqXHR) {
             console.log("HTTP Request Succeeded: " + jqXHR.status);
             console.log(data);
-            turn_on_ac1_changeImage()
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             console.log("HTTP Request Failed");
@@ -38,47 +30,33 @@ function turn_on_ac() {
         .always(function () {
             /* ... */
         });
-    //
-
-}
-
-function turn_on_ac1_changeImage() {
-    document.getElementById('ac1').src = 'https://www.fotoaparatas.lt/images/thumbs/thumb-3-55x30m-green-screen-349177-640-448.png'
 }
 
 // Off
-function turn_off_ac() {
-    console.log("Hello");
+function turn_off_ac(device_id) {
+    console.log("turn_off_ac" + device_id);
     //
     console.log("POST method by jQuery");
     jQuery.ajax({
-        // url: "https://cors-anywhere.herokuapp.com/https://peahivebackend.herokuapp.com/hiveapi/",
-        url: "https://cors-anywhere.herokuapp.com/https://hookb.in/2qMV6mpxeDiDYDK60pRq",
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
         type: "POST",
         headers: {
-            // "Authorization": "Token d6c0e1dd9df3d71a3b21d70959bf0857859c4199",
-            "Authorization": "Token dscnsjdkcnsjdkcnjsdcnjdsncjsdnckjsdnckjsdcnkjsdcnskjdcnjksdncbs",
-            "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://hookb.in/2qMV6mpxeDiDYDK60pRq",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
             "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
-        contentType: "application/json",
-        data: JSON.stringify({
-            "topic": "Test post device control",
-            "type": "devicecontrol",
-            "message": {
-                "device": "MSA Device control",
-                "parameter": {
-                    "status": "on"
-                }
-            }
-        })
+        contentType: "application/x-www-form-urlencoded",
+        data: {
+            "device_type": "AC",
+            "device_id": device_id,
+            "status": "OFF",
+        },
     })
         .done(function (data, textStatus, jqXHR) {
             console.log("HTTP Request Succeeded: " + jqXHR.status);
             console.log(data);
-            turn_off_ac1_changeImage()
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             console.log("HTTP Request Failed");
@@ -86,12 +64,57 @@ function turn_off_ac() {
         .always(function () {
             /* ... */
         });
-    //
-
 }
 
-function turn_off_ac1_changeImage() {
-    document.getElementById('ac1').src = 'images/goodbye.jpg'
+// Set Temp
+var score = 25
+function up_remote1() {
+    if (score < 30) {
+        score++;
+    }
+    document.getElementById("result").innerHTML = score;
+    return(score);
+}
+
+function down_remote1() {
+    if (score > 17) {
+        score--;
+    }
+    document.getElementById("result").innerHTML = score;
+    return(score);
+}
+
+function Adj_Temp() {
+    console.log("Adjust AC Temp: " + score + "degreeC")
+    jQuery.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
+        type: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+        contentType: "application/x-www-form-urlencoded",
+        data: {
+            "device_type": "AC",
+            "device_id": device_id,
+            "status": "OFF",
+        },
+        })
+        .done(function (data, textStatus, jqXHR) {
+            console.log("HTTP Request Succeeded: " + jqXHR.status);
+            console.log(data);
+            var Status_smart_plug_1_Local = localStorage.setItem("Status_smart_plug_1_Local", Status_smart_plug_1)
+            smartplug1_changeImage(Status_smart_plug_1)            
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("HTTP Request Failed");
+        })
+        .always(function () {
+            /* ... */
+        });
 }
 
 // Submit Temp
@@ -138,8 +161,8 @@ function TestLogin() {
 
 }
 
-
-
+// TODO //
+// --------------------------------------- //
 // CoolModeControl
 function CoolModeControl() {
     console.log("Hello");
@@ -571,21 +594,4 @@ function speedfive() {
     //
 
 }
-
-// Set Temp
-var score = 25
-function up_remote1() {
-    if (score < 30) {
-        score++;
-    }
-    document.getElementById("result").innerHTML = score;
-}
-
-function down_remote1() {
-    if (score > 17) {
-        score--;
-    }
-    document.getElementById("result").innerHTML = score;
-}
-// 
 
