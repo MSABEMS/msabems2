473f38298fcfae085c087db03064f7d7dcc2771f
PEAMSA <peasbh@gmail.com>
2020-03-03 16:08:29 +0700

    Edit Smart Plug page

--- js/SmartPlug/SmartPlug.js
+++ js/SmartPlug/SmartPlug.js
@@ -7,7 +7,7 @@ ref_smart_plug_1.on("value", function(snapshot) {
     console.log("Get smart_plug_1 from firebase")
     var smart_plug_1 = snapshot.val();
     // console.log(smart_plug_1["CURRENT"])
-    document.getElementById("kW_Plug_1").innerHTML = smart_plug_1["CURRENT"].toFixed(2);
+    document.getElementById("kW_Plug_1").innerHTML = smart_plug_1["POWER"].toFixed(2);
     document.getElementById("V_Plug_1").innerHTML = smart_plug_1["VOLTAGE"];
     document.getElementById("I_Plug_1").innerHTML = smart_plug_1["CURRENT"].toFixed(2);
     // Recheck Device Status from Firebase //
@@ -82,7 +82,7 @@ ref_smart_plug_2.on("value", function(snapshot) {
     console.log("Get smart_plug_2 from firebase")
     var smart_plug_2 = snapshot.val();
     // console.log(smart_plug_1["CURRENT"])
-    document.getElementById("kW_Plug_2").innerHTML = smart_plug_2["CURRENT"].toFixed(2);
+    document.getElementById("kW_Plug_2").innerHTML = smart_plug_2["POWER"].toFixed(2);
     document.getElementById("V_Plug_2").innerHTML = smart_plug_2["VOLTAGE"];
     document.getElementById("I_Plug_2").innerHTML = smart_plug_2["CURRENT"].toFixed(2);
     // Recheck Device Status from Firebase //
@@ -149,7 +149,6 @@ function smartplug2(devieID_smart_plug_2) {
 
 }
 
-
 //Smart Plug 3
 // Retrieve data from Firebase for Checking Device Status
 var ref_smart_plug_3 = ref_devices_data.child("SP101003");
@@ -158,7 +157,7 @@ ref_smart_plug_3.on("value", function(snapshot) {
     console.log("Get smart_plug_3 from firebase")
     var smart_plug_3 = snapshot.val();
     // console.log(smart_plug_1["CURRENT"])
-    document.getElementById("kW_Plug_3").innerHTML = smart_plug_3["CURRENT"].toFixed(2);
+    document.getElementById("kW_Plug_3").innerHTML = smart_plug_3["POWER"].toFixed(2);
     document.getElementById("V_Plug_3").innerHTML = smart_plug_3["VOLTAGE"];
     document.getElementById("I_Plug_3").innerHTML = smart_plug_3["CURRENT"].toFixed(2);
     // Recheck Device Status from Firebase //
@@ -170,7 +169,7 @@ ref_smart_plug_3.on("value", function(snapshot) {
 
 // Change Image for Smart Plug
 function smartplug3_changeImage(Status_smart_plug_3) {
-    var image = document.getElementById('myImage2');
+    var image = document.getElementById('myImage3');
     if (Status_smart_plug_3 == "OFF") {
         image.src = "images/remodeair/switchoff.png";
     } else {
@@ -193,18 +192,16 @@ function smartplug3(devieID_smart_plug_3) {
     //
     console.log("POST method by jQuery");
     jQuery.ajax({
-        // url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug/",
-        url: "https://cors-anywhere.herokuapp.com/https://hookb.in/Z2loLbOlQlt1MVqkb7aY",
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
         type: "POST",
         headers: {
-            // "Authorization": "Token d6c0e1dd9df3d71a3b21d70959bf0857859c4199",
+            "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
-            "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://hookb.in/Z2loLbOlQlt1MVqkb7aY",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
             "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
-        contentType: "application/json",
+        contentType: "application/x-www-form-urlencoded",
         data: JSON.stringify({
             "device_type": "plug",
             "device_id": "plug003",
@@ -235,7 +232,7 @@ ref_smart_plug_4.on("value", function(snapshot) {
     console.log("Get smart_plug_4 from firebase")
     var smart_plug_4 = snapshot.val();
     // console.log(smart_plug_1["CURRENT"])
-    document.getElementById("kW_Plug_4").innerHTML = smart_plug_4["CURRENT"].toFixed(2);
+    document.getElementById("kW_Plug_4").innerHTML = smart_plug_4["POWER"].toFixed(2);
     document.getElementById("V_Plug_4").innerHTML = smart_plug_4["VOLTAGE"];
     document.getElementById("I_Plug_4").innerHTML = smart_plug_4["CURRENT"].toFixed(2);
     // Recheck Device Status from Firebase //
@@ -270,18 +267,16 @@ function smartplug4(devieID_smart_plug_4) {
     //
     console.log("POST method by jQuery");
     jQuery.ajax({
-        // url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug/",
-        url: "https://cors-anywhere.herokuapp.com/https://hookb.in/Z2loLbOlQlt1MVqkb7aY",
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
         type: "POST",
         headers: {
-            // "Authorization": "Token d6c0e1dd9df3d71a3b21d70959bf0857859c4199",
+            "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
-            "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://hookb.in/Z2loLbOlQlt1MVqkb7aY",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
             "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
-        contentType: "application/json",
+        contentType: "application/x-www-form-urlencoded",
         data: JSON.stringify({
             "device_type": "plug",
             "device_id": "plug004",
@@ -312,7 +307,7 @@ ref_smart_plug_5.on("value", function(snapshot) {
     console.log("Get smart_plug_5 from firebase")
     var smart_plug_5 = snapshot.val();
     // console.log(smart_plug_1["CURRENT"])
-    document.getElementById("kW_Plug_5").innerHTML = smart_plug_5["CURRENT"].toFixed(2);
+    document.getElementById("kW_Plug_5").innerHTML = smart_plug_5["POWER"].toFixed(2);
     document.getElementById("V_Plug_5").innerHTML = smart_plug_5["VOLTAGE"];
     document.getElementById("I_Plug_5").innerHTML = smart_plug_5["CURRENT"].toFixed(2);
     // Recheck Device Status from Firebase //
@@ -347,18 +342,16 @@ function smartplug5(devieID_smart_plug_5) {
     //
     console.log("POST method by jQuery");
     jQuery.ajax({
-        // url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug/",
-        url: "https://cors-anywhere.herokuapp.com/https://hookb.in/Z2loLbOlQlt1MVqkb7aY",
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
         type: "POST",
         headers: {
-            // "Authorization": "Token d6c0e1dd9df3d71a3b21d70959bf0857859c4199",
+            "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
-            "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://hookb.in/Z2loLbOlQlt1MVqkb7aY",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
             "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
-        contentType: "application/json",
+        contentType: "application/x-www-form-urlencoded",
         data: JSON.stringify({
             "device_type": "plug",
             "device_id": "plug005",
@@ -370,6 +363,81 @@ function smartplug5(devieID_smart_plug_5) {
             console.log(data);
             var Status_smart_plug_5_Local = localStorage.setItem("Status_smart_plug_5_Local", Status_smart_plug_5)
             smartplug5_changeImage(Status_smart_plug_5)
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("HTTP Request Failed");
+        })
+        .always(function () {
+            /* ... */
+        });
+    //
+
+}
+
+//Smart Plug 6
+// Retrieve data from Firebase for Checking Device Status
+var ref_smart_plug_6 = ref_devices_data.child("SP101006");
+
+ref_smart_plug_6.on("value", function(snapshot) {
+    console.log("Get smart_plug_6 from firebase")
+    var smart_plug_6 = snapshot.val();
+    // console.log(smart_plug_1["CURRENT"])
+    document.getElementById("kW_Plug_6").innerHTML = smart_plug_6["POWER"].toFixed(2);
+    document.getElementById("V_Plug_6").innerHTML = smart_plug_6["VOLTAGE"];
+    document.getElementById("I_Plug_6").innerHTML = smart_plug_6["CURRENT"].toFixed(2);
+    // Recheck Device Status from Firebase //
+    var Status_smart_plug_6 = smart_plug_6["STATUS"];
+    console.log(Status_smart_plug_6);
+    smartplug6_changeImage(Status_smart_plug_6)
+    var Status_smart_plug_6_Local = localStorage.setItem("Status_smart_plug_6_Local", Status_smart_plug_6)
+  });
+
+// Change Image for Smart Plug
+function smartplug6_changeImage(Status_smart_plug_6) {
+    var image = document.getElementById('myImage6');
+    if (Status_smart_plug_6 == "OFF") {
+        image.src = "images/remodeair/switchoff.png";
+    } else {
+        image.src = "images/remodeair/switchon.png";
+    }
+}
+
+//  On/Off Smart Plug 6
+function smartplug6(devieID_smart_plug_6) {
+    console.log("-----Control_Smart_Plug_6-----");
+    console.log(devieID_smart_plug_6)
+    console.log(localStorage.getItem("Status_smart_plug_6_Local"))
+    //
+    var Status_smart_plug_6 = localStorage.getItem("Status_smart_plug_6_Local")
+    if (Status_smart_plug_6 == "OFF") {
+        Status_smart_plug_6 = "ON"
+    } else {
+        Status_smart_plug_6 = "OFF"
+    }
+    //
+    console.log("POST method by jQuery");
+    jQuery.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
+        type: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
+            "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/plug",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+        contentType: "application/x-www-form-urlencoded",
+        data: JSON.stringify({
+            "device_type": "plug",
+            "device_id": "plug006",
+            "status": Status_smart_plug_6,
+        })
+    })
+        .done(function (data, textStatus, jqXHR) {
+            console.log("HTTP Request Succeeded: " + jqXHR.status);
+            console.log(data);
+            var Status_smart_plug_6_Local = localStorage.setItem("Status_smart_plug_6_Local", Status_smart_plug_6)
+            smartplug6_changeImage(Status_smart_plug_6)
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             console.log("HTTP Request Failed");
